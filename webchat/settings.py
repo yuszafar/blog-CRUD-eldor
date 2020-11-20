3a5fe4d5fcb01d0616da3f1b897c4c241eff1ac4
Amir Mullagaliev <amirmullagaliev@gmail.com>
2020-11-20 14:57:00 +0500

    integrate the Channels library to run server and handle http requests

--- webchat/settings.py
+++ webchat/settings.py
@@ -31,6 +31,7 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    'channels',
     'chat',
     'django.contrib.admin',
     'django.contrib.auth',
@@ -119,3 +120,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.1/howto/static-files/
 
 STATIC_URL = '/static/'
+
+# Channels
+
+ASGI_APPLICATION = 'webchat.asgi.application'
